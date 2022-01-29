d088bb422c057384d6247d9e27ed06bb0f440d05
Maksymilian Manczak <maksymilianmanczak@gmail.com>
2022-01-29 15:03:39 +0100

    contact page

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const contactBtn = document.getElementById("contact-btn");
+
+contactBtn.addEventListener("click", () => {
+  alert("Thanks for your comment!");
+});
+
 // Sidebar
 const toggleBtn = document.querySelector(".sidebar-toggle");
 const closeBtn = document.querySelector(".close-btn");
@@ -10,4 +16,12 @@ toggleBtn.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
 });
-// Sidebar
+
+function ifChecked() {
+  const checkbox = document.getElementById("checkbox").checked;
+  if (checkbox === true) {
+    console.log("yes");
+  } else {
+    console.log("no");
+  }
+}
